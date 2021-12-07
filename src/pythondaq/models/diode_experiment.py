daf7a6e822aa89122bbc99bea53e33266a25985b
JukkaJohn <jukkajohn2002@gmail.com>
2021-12-07 19:02:11 +0100

    ItFinallyWorks

--- src/pythondaq/models/diode_experiment.py
+++ src/pythondaq/models/diode_experiment.py
@@ -11,112 +11,131 @@ import threading
 
 class DiodeExperiment:
     def __init__(self, port):
+        """opens the selected device
+
+        Args:
+            port (str): selected device
+        """
         self.device = ArduinoVISADevice(port)
         self.port = port
 
         self.I = []
         self.U_led = []
 
-    def list(self, search):
-        """[summary]
-        Args:
-            search ([type]): [description]
-        Returns:
-            [type]: [description]
-        """
-        if str(search) in str(list_devices()):
-            for item in list_devices():
-                if str(search) in str(item):
-                    return f"The following devices match your search string: {item}"
+    # def list(self, search):
+    #     """[summary]
+    #     Args:
+    #         search ([type]): [description]
+    #     Returns:
+    #         [type]: [description]
+    #     """
+    #     if str(search) in str(list_devices()):
+    #         for item in list_devices():
+    #             if str(search) in str(item):
+    #                 return f"The following devices match your search string: {item}"
 
-        else:
-            return (
-                f"The following devices are connected to your computer:{list_devices()}"
-            )
+    #     else:
+    #         return (
+    #             f"The following devices are connected to your computer:{list_devices()}"
+    #         )
 
-    def info(self, search):
-        """[summary]
-        Args:
-            search ([type]): [description]
-        Returns:
-            [type]: [description]
-        """
-        if str(search) in str(list_devices()):
-            for item in list_devices():
-                if str(search) in str(item):
-                    return f"The following devices match your search string:{self.device.get_identification()}"
+    # def info(self, search):
+    #     """[summary]
+    #     Args:
+    #         search ([type]): [description]
+    #     Returns:
+    #         [type]: [description]
+    #     """
+    #     if str(search) in str(list_devices()):
+    #         for item in list_devices():
+    #             if str(search) in str(item):
+    #                 return f"The following devices match your search string:{self.device.get_identification()}"
 
-        else:
-            return "There are no devices"
+    #     else:
+    #         return "There are no devices"
 
-    def current(self, voltage):
-        """[summary]
-        Args:
-            voltage ([type]): [description]
-        Returns:
-            [type]: [description]
-        """
-        I = []
-        self.device.set_output_voltage(channel=0, value=voltage)
-        volt_ch2 = self.device.get_input_voltage(channel=2)
-        I.append(float(volt_ch2) / 220)
-        self.device.set_output_value(channel=0, value=0)
-        return I
+    # def current(self, voltage):
+    #     """[summary]
+    #     Args:
+    #         voltage ([type]): [description]
+    #     Returns:
+    #         [type]: [description]
+    #     """
+    #     I = []
+    #     self.device.set_output_voltage(channel=0, value=voltage)
+    #     volt_ch2 = self.device.get_input_voltage(channel=2)
+    #     I.append(float(volt_ch2) / 220)
+    #     self.device.set_output_value(channel=0, value=0)
+    #     return I
 
     def close(self):
+        """Closes the window
+        """
         self.device.closes()
+
+    def start_scan(self, start, stop, steps):
+        """Start a new thread to execute a scan."""
+        self._scan_thread = threading.Thread(
+            target=self.scan, args=(start, stop, steps)
+        )
+        self._scan_thread.start()
 
     def scan(self, begin_range, end_range, counts):
 
-        """[summary]
+        """excecutes measurments
         Args:
-            begin_range ([type]): [description]
-            end_range ([type]): [description]
-            output ([type]): [description]
-            counts ([type]): [description]
+            begin_range (float): the start of range of measurements
+            end_range (float): the end of range of measurements
+            counts (int): number of times one value in the range is measured
         Returns:
-            [type]: [description]
+            int: voltages, currents and their errors
         """
         self.U_led = []
         self.I = []
         error_I_mean_list = []
         error_U_led_list = []
+        # loops over the range of voltages
         for value in np.arange(begin_range, end_range, 0.03):
             mean_U_led = 0
             mean_I = 0
+            # stores data with a loop over input voltage
             voltages = []
             currents = []
-
+            # loops a number of times over one voltage
             for i in range(0, counts):
+                # measurements
                 self.device.set_output_voltage(channel=0, value=value)
                 volt_ch1 = self.device.get_input_voltage(channel=1)
                 volt_ch2 = self.device.get_input_voltage(channel=2)
 
+                # calculates voltage and current over LED
                 mean_U_led += (float(volt_ch1) - float(volt_ch2)) / counts
                 mean_I += (float(volt_ch2) / 220) / counts
 
                 voltages.append(float(volt_ch1) - float(volt_ch2))
                 currents.append(float(volt_ch2) / 220)
                 i += 1
-                # print(value, volt_ch1, volt_ch2)
 
             self.U_led.append(mean_U_led)
             self.I.append(mean_I)
+            # calculate error
             err_mean_U = stdev(voltages) / np.sqrt(counts)
             err_mean_I = stdev(currents) / np.sqrt(counts)
 
             error_U_led_list.append(err_mean_U)
             error_I_mean_list.append(err_mean_I)
-
+        # stops measurements
         self.device.set_output_value(channel=0, value=0)
         return self.U_led, self.I, error_I_mean_list, error_U_led_list
 
     def save(self, output):
+        """Saves the data from the measurements
 
-        self.I
-        self.U_led
+        Args:
+            output (str): name of the csv file
+        """
         if output != "":
-            with open(f"{output}.csv", "w") as f:
+            with open(f"{output}", "w") as f:
                 writer = csv.writer(f)
                 writer.writerow(["U_LED", "I"])
                 for u, i in zip(self.U_led, self.I):
--- src/pythondaq/views/application.py
+++ src/pythondaq/views/application.py
@@ -11,25 +11,32 @@ pg.setConfigOption("foreground", "k")
 
 
 class UserInterface(QtWidgets.QMainWindow):
+    """Creates a graphical user interface
+
+    Args:
+        QtWidgets: Creates an external window with graphical widgets
+    """
+
     def __init__(self):
+        """This function contains buttons and makes a layout.
+        """
         self.device = None
 
-        # roep de __init__() aan van de parent class
+        # call __init__() from parent class
         super().__init__()
         connected_devices = list_devices()
-        # hierbinnen maak je een layout en hang je andere widgets
+        # within this it creates a layout. Within the layout widgets are created
         central_widget = QtWidgets.QWidget()
         self.setCentralWidget(central_widget)
-        # voeg geneste layouts en widgets toe
+        # add layouts and widgets
         vbox = QtWidgets.QVBoxLayout(central_widget)
-        # self.textedit = QtWidgets.QTextEdit()
-        # vbox.addWidget(self.textedit)
         hbox = QtWidgets.QHBoxLayout()
         vbox.addLayout(hbox)
 
         self.plot_widget = pg.PlotWidget()
         hbox.addWidget(self.plot_widget)
 
+        # This button determines the start of range of measurements
         self.start_button = QtWidgets.QDoubleSpinBox()
         hbox.addWidget(self.start_button)
         self.start_button.valueChanged.connect(self.change)
@@ -37,6 +44,7 @@ class UserInterface(QtWidgets.QMainWindow):
         self.start_button.setMaximum(3.3)
         self.start_button.setFixedWidth(60)
 
+        # This button determines the end of range of measurements
         self.end_button = QtWidgets.QDoubleSpinBox()
         hbox.addWidget(self.end_button)
         self.end_button.valueChanged.connect(self.change)
@@ -44,6 +52,7 @@ class UserInterface(QtWidgets.QMainWindow):
         self.end_button.setMinimum(0)
         self.end_button.setFixedWidth(60)
 
+        # This button determines the number of times one value in the range is measured
         self.point_button = QtWidgets.QSpinBox()
         hbox.addWidget(self.point_button)
         self.point_button.valueChanged.connect(self.change)
@@ -51,90 +60,113 @@ class UserInterface(QtWidgets.QMainWindow):
         self.point_button.setMaximum(10)
         self.point_button.setFixedWidth(60)
 
+        # Layout of start, end and point button
         self.formlayout = QtWidgets.QFormLayout()
         self.formlayout.addRow(self.tr("&start"), self.start_button)
         self.formlayout.addRow(self.tr("&stop"), self.end_button)
         self.formlayout.addRow(self.tr("&step"), self.point_button)
         hbox.addLayout(self.formlayout)
 
+        # This button starts the mesasurements
         self.start_measurement = QtWidgets.QPushButton("Measure")
         self.start_measurement.setFixedWidth(300)
         hbox.addWidget(self.start_measurement)
-        self.start_measurement.clicked.connect(self.measure)
+        self.start_measurement.clicked.connect(self.start_scan)
         self.start_measurement.setFixedWidth(120)
 
+        # This button closes the window
         self.quit = QtWidgets.QPushButton("quit")
         self.quit.setFixedWidth(300)
         self.quit.clicked.connect(self.shut)
         hbox.addWidget(self.quit)
         self.quit.setFixedWidth(120)
 
+        # This button saves the data from the measurements
         self.save = QtWidgets.QPushButton("save")
         self.save.setFixedWidth(300)
         self.save.clicked.connect(self.save_data)
         hbox.addWidget(self.save)
         self.save.setFixedWidth(120)
 
+        # Layout from start, quit and save buttons
         self.formlayout = QtWidgets.QHBoxLayout()
         self.formlayout.addWidget(self.start_measurement)
         self.formlayout.addWidget(self.quit)
         self.formlayout.addWidget(self.save)
         vbox.addLayout(self.formlayout)
 
+        # This button lets you choose the device
         self.devices = QtWidgets.QComboBox()
         self.devices.addItems(connected_devices)
         self.devices.currentTextChanged.connect(self.open)
         hbox.addWidget(self.devices)
         self.devices.setFixedWidth(120)
 
+        # Plot timer
+        self.plot_timer = QtCore.QTimer()
+        # Calls the measurement function every 100 ms
+        self.plot_timer.timeout.connect(self.plot)
+        self.plot_timer.start(100)
+
     def open(self):
+        """This function opens the selected device
+
+        Returns:
+            The opened device
+        """
         if self.device != None:
             self.device.close()
         self.device = DiodeExperiment(self.devices.currentText())
         return self.device
 
     def save_data(self):
+        """This function calls another function to save the data
+        """
         filename, _ = QtWidgets.QFileDialog.getSaveFileName(filter="CSV files (*.csv)")
         self.device.save(filename)
 
     def change(self):
+        """This function stores the input values of the start, end and point button
+
+        Returns:
+            The input values
+        """
         start_value = self.start_button.value()
         end_value = self.end_button.value()
         step_value = self.point_button.value()
 
         return start_value, end_value, step_value
 
-    def measure(self):
+    def start_scan(self):
+        """This function calls another function, which excecutes measurements and gives data.
+        """
         self.device = DiodeExperiment(self.devices.currentText())
+        self.device.start_scan(
+            self.start_button.value(),
+            self.end_button.value(),
+            self.point_button.value(),
+        )
+
+    def plot(self):
+        """This function plots the data from the measurements
+        """
         self.plot_widget.clear()
-        self.U_led, _, _, _ = self.device.scan(
-            self.start_button.value(),
-            self.end_button.value(),
-            self.point_button.value(),
-        )
-        _, self.I, _, _ = self.device.scan(
-            self.start_button.value(),
-            self.end_button.value(),
-            self.point_button.value(),
-        )
-        print(self.U_led)
-        print(self.I)
-        print(len(self.U_led))
         self.plot_widget.plot(
-            self.device.U_led,
-            self.device.I,
-            symbol=None,
-            pen={"color": "k", "width": 5},
+            self.device.U_led, self.device.I, symbol="o", symbolSize=5, pen=None,
         )
 
         self.plot_widget.setLabel("left", "current(I)")
         self.plot_widget.setLabel("bottom", "voltage(U)")
 
     def shut(self):
+        """Closes the window
+        """
         self.close()
 
 
 def main():
+    """This function launches the external window
+    """
     app = QtWidgets.QApplication(sys.argv)
     ui = UserInterface()
     ui.show()
